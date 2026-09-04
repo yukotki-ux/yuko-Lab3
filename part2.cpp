5cd7efa8c59c64e4bc77f1f08cfe0414dd268b18
yukotki-ux <11102218@cc.peralta.edu>
2026-09-04 21:26:04 +0000

    9.4.26

--- part2.cpp
+++ part2.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    
+    int x;
+
+    cout << "\nLet's enter your favorite integer!: " << endl;
+    cin >> x;
+
+    x = ( x * 2 + 10 ) / 2 - x;
+
+    cout << "\n\nYou can't escape from the number..." << x << "!" << endl;
 
 
     return 0;
