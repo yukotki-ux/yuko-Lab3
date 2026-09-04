5ca1610a714f52acbdb750c0bcf345d99874bca0
yukotki-ux <11102218@cc.peralta.edu>
2026-09-04 23:56:05 +0000

    9.4.26

--- part4.cpp
+++ part4.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
+    int numOfPeople;
+    int slicesPerPizza;
+    int slicesPerPerson;
 
+    cout << "\nHow many people will be attending to the pizza party? ";
+    cin >> numOfPeople;
+    cout << "\nHow many slices of pizza are included in each pizza? ";
+    cin >> slicesPerPizza;
+    cout << "\nHow many slices of pizza will each person eat? ";
+    cin >> slicesPerPerson;
 
+    int totalSlicesPizza = numOfPeople * slicesPerPerson;
+    int totalWholePizza = totalSlicesPizza / slicesPerPizza + 1;
 
+    int leftOverSlices = totalWholePizza * slicesPerPizza % totalSlicesPizza;
 
-
-
-
-
-
-    cout << "There will be " << leftOverSlices << "leftover slices."
+    cout << "\n\nYou will need to order " << totalWholePizza << " pizzas.";
+    cout << "\nThere will be " << leftOverSlices << " leftover slices." << endl;
     
     return 0;
 }
